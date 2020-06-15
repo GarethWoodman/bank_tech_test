c67f45608eaeac93214327851ce0360e35ea1131
GarethWoodman <gareth.woodman92@gmail.com>
2020-06-15 15:12:04 +0100

    Submitted values with two decimal places

--- spec/account_statement_spec.rb
+++ spec/account_statement_spec.rb
@@ -8,18 +8,20 @@ describe AccountStatement do
 
   context 'few transactions made' do
     let(:make_transactions) do
-      atm.deposit(50)
-      atm.deposit(10)
-      atm.withdraw(20)
+      atm.deposit(50.00)
+      atm.deposit(10.00)
+      atm.withdraw(20.00)
     end
 
     it 'prints account statement' do
       make_transactions
 
       statement =  "date || credit || debit || balance\n"
-      statement += "#{DATE} ||  || #{50} || #{0}\n"
-      statement += "#{DATE} ||  || #{10} || #{50}\n"
-      statement += "#{DATE} || #{20} ||  || #{60}\n"
+      statement += "#{DATE} ||  || #{50.00} || #{0}\n"
+      statement += "#{DATE} ||  || #{10.00} || #{50.00}\n"
+      statement += "#{DATE} || #{20.00} ||  || #{60.00}\n"
+
+      puts statement
 
       expect(account_statement.print).to eq statement
     end
