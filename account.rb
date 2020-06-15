dccc4cf7f79ea8dad4258bbf57f0b72ec9873760
GarethWoodman <gareth.woodman92@gmail.com>
2020-06-15 11:14:25 +0100

    @transactions is empty by default

--- account.rb
+++ account.rb
@@ -1,6 +1,7 @@
 class Account
-  attr_reader :balance
+  attr_reader :balance, :transactions
   def initialize
     @balance = 0
+    @transactions = []
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,9 +1,11 @@
 require './account'
 
 describe Account do
-  context 'Attributes' do
-    it 'has a default balance of 0' do
-      expect(subject.balance).to eq 0
-    end
+  it 'has a default balance of 0' do
+    expect(subject.balance).to eq 0
+  end
+
+  it 'has no transactions by default' do
+    expect(subject.transactions).to eq []
   end
 end
