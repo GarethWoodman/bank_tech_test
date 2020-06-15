f4ed911220af1d6558857b23a0cab9302f31f36f
GarethWoodman <gareth.woodman92@gmail.com>
2020-06-15 14:51:17 +0100

    #print an account statement

---
+++ account_statement.rb
@@ -0,0 +1,14 @@
+class AccountStatement
+  def initialize(account)
+    @account = account
+  end
+
+  def print
+    statement = "date || credit || debit || balance\n"
+    @account.transactions.each do |transaction|
+      statement += transaction.join(" || ")
+      statement += "\n"
+    end
+    statement
+  end
+end
---
+++ spec/account_statement_spec.rb
@@ -0,0 +1,27 @@
+require './account_statement'
+DATE = Time.now.strftime("%d/%m/%Y")
+
+describe AccountStatement do
+  let(:account)           { Account.new }
+  let(:atm)               { Atm.new(account) }
+  let(:account_statement) { AccountStatement.new(account)}
+
+  context 'few transactions made' do
+    let(:make_transactions) do
+      atm.deposit(50)
+      atm.deposit(10)
+      atm.withdraw(20)
+    end
+
+    it 'prints account statement' do
+      make_transactions
+
+      statement =  "date || credit || debit || balance\n"
+      statement += "#{DATE} ||  || #{50} || #{0}\n"
+      statement += "#{DATE} ||  || #{10} || #{50}\n"
+      statement += "#{DATE} || #{20} ||  || #{60}\n"
+
+      expect(account_statement.print).to eq statement
+    end
+  end
+end
